70ae2b6e0f933eaa45252249539971f80d87b9a7
Robert Ciszek <ciszekrm@vcu.edu>
2021-04-26 10:27:29 -0400

    publishing info

---
+++ Automation/Publish.bat
@@ -0,0 +1,2 @@
+"C:\Users\Robby\Downloads\butler-windows-amd64\butler.exe"  push "F:\Projects\GitRepos\LD_48\Exports\WindowsNoEditor" jimmothysanchez/deepest-darkest-double-bogey:win --userversion 1.0.0
+pause
